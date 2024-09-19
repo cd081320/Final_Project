b9bfd1f3a689b25a19b6369de19fba3d2bae459a
김정현 <93852478+cd081320@users.noreply.github.com>
2024-09-19 16:45:27 +0900

    Delete ChoDanGi directory

--- ChoDanGi/src/com/sist/chodangi/IAdminDAO.java
+++
@@ -1,9 +0,0 @@
-/*
- * IAdminDAO.java
- */
-package com.sist.chodangi;
-
-public interface IAdminDAO
-{
-	public int login(AdminDTO admin);
-}
--- ChoDanGi/src/com/sist/chodangi/SeekerDTO.java
+++
@@ -1,6 +0,0 @@
-package com.sist.chodangi;
-
-public class SeekerDTO
-{
-
-}
